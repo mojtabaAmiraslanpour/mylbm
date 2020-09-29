4c91b69491bc8d482fb36e57be368eb381e31b2a
Mojtaba <amiraslanpoor@gmail.com>
2020-09-29 12:21:27 +0330

    Streaming

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mojtaba/Desktop/MyCode/mylbm
-# Build directory: /home/mojtaba/Desktop/MyCode/mylbm/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
